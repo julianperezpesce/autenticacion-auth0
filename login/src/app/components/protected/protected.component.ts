36f0679fce11fb8c8d3e92866e88b46ab068f187
julian <estrotococoa@gmail.com>
2020-09-04 17:41:40 -0300

    final commit

--- login/src/app/components/protected/protected.component.ts
+++ login/src/app/components/protected/protected.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-protected',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProtectedComponent implements OnInit {
 
-  constructor() { }
+  constructor( public auth: AuthService ) { }
 
   ngOnInit(): void {
   }
